d8eed357842a95b5ad7a763f0935419ecc95d523
Tommi Kiiski <kiiski.tommi@hotmail.com>
2022-11-08 13:18:47 +0200

    Updated gitignore and created more tests (completed until task 8)

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,22 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alustuu_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(200)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 12.00 euroa")
+
+    def test_saldo_vahenee_oikein(self):
+        self.maksukortti.ota_rahaa(200)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1200), False)
+
+        
+
