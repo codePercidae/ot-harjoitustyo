4d48db55a756c2528c8cb56727dd9ed8a9617af9
Tommi Kiiski <kiiski.tommi@hotmail.com>
2022-11-29 16:36:07 +0200

    Create more tests

--- src/tests/repository_test.py
+++ src/tests/repository_test.py
@@ -1,10 +1,17 @@
 import unittest
 from question_repository import QuestionRepository
 
+
 class TestRepository(unittest.TestCase):
     def setUp(self):
-        self.repositry = QuestionRepository()
+        self.repository = QuestionRepository()
+        self.repository.initialize_database()
 
-    def test_add_question_adds_new_question_to_dictionary(self):
-        self.repositry.add_question("New question")
-        self.assertEqual(self.repositry.questions["New question"], [])
+    def test_add_question_adds_new_question_to_database(self):
+        self.repository.add_question("New question")
+        self.assertEqual(self.repository.get_questions()[0][1], "New question")
+
+    def test_new_grade_sets_correct_grade(self):
+        self.repository.add_question("New question")
+        self.repository.new_grade(1, 5)
+        self.assertEqual(self.repository.get_values(1)[0], 5)
