6c019fa17851546642164e29fe881e2e3156b682
Tommi Kiiski <kiiski.tommi@hotmail.com>
2022-12-06 21:15:07 +0200

    Increase test coverage

--- src/tests/repository_test.py
+++ src/tests/repository_test.py
@@ -6,12 +6,21 @@ class TestRepository(unittest.TestCase):
     def setUp(self):
         self.repository = QuestionRepository()
         self.repository.initialize_database()
+        self.repository.add_question("New question")
 
     def test_add_question_adds_new_question_to_database(self):
-        self.repository.add_question("New question")
         self.assertEqual(self.repository.get_questions()[0][1], "New question")
 
+    def test_add_question_return_false_if_question_already_exists(self):
+        self.assertEqual(self.repository.add_question("New question"), False)
+
     def test_new_grade_sets_correct_grade(self):
-        self.repository.add_question("New question")
         self.repository.new_grade(1, 5)
         self.assertEqual(self.repository.get_values(1)[0], 5)
+
+    def test_get_questions_functions_correctly(self):
+        self.assertEqual(self.repository.get_questions(), [(1, "New question")])
+
+    def test_initalize_database_clears_all_data(self):
+        self.repository.initialize_database()
+        self.assertEqual(self.repository.get_questions(), [])
